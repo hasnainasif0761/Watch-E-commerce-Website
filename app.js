0f4a89a311c2b7ca3d19969251049872a7a64575
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-22 18:23:25 +0500

    11-Updation

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var ham = document.querySelector(".hamburger");
 var navLinks = document.querySelector(".nav-links");
+var incr = document.getElementById("incr");
 
 ham.addEventListener('click',()=>{
     navLinks.classList.toggle("open");
 })
+
+function ShowAlert(){
+    alert("Item Added to Cart Successfully 🎉");
+}
+
+function incrementCart(){
+    let count = parseInt(incr.innerText) || 0;
+    count++;
+    incr.innerText = count;
+}
