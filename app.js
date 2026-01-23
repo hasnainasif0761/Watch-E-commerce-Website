3312e3c35143bab1b73bb45bf2e916eec2858199
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-01-23 07:50:34 +0500

    12-Updation

--- app.js
+++ app.js
@@ -15,3 +15,37 @@ function incrementCart(){
     count++;
     incr.innerText = count;
 }
+
+function swapImage(img, imgSrc) {
+    var image = document.getElementById(img);
+    image.src = imgSrc
+}
+
+var ExpandPara = false;
+function showPara() {
+    var para = document.getElementById("showPara");
+    if(!ExpandPara){
+        para.innerHTML = ` Quis, animi architecto iste aspernatur, iure officia dolores veritatis praesentium voluptatem dolorem fugit earum reiciendis fuga temporibus cumque excepturi atque facere minus!<a href="#" onclick="showPara()">Hide para</a>`
+        ExpandPara = true;
+    }else{
+        para.innerHTML = `Lorem, ipsum dolor sit amet consectetur adipisicing elit. <a href="#" onclick="showPara()">Show Para</a>`    
+        ExpandPara = false;
+    }
+
+}
+
+function changeImage(imgId, imgSrc){
+    var image = document.getElementById(imgId);
+
+    if (image.style.display === 'none') {
+        image.style.display = 'block';
+        image.src = imgSrc;
+    } else {
+        image.style.display = 'none';
+    }
+}
+
+
+
+
+
